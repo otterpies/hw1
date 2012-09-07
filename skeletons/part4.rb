8570da0eee914c80b3326c45b43087bcaea345ca
Peter Tsoi <petertsoi@gmail.com>
2012-09-06 19:00:26 -0700

    First pass at part 4: basic OOP

--- skeletons/part4.rb
+++ skeletons/part4.rb
@@ -1,23 +1,27 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
-    # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
-    # YOUR CODE HERE
+    return calories < 200
   end
   
   def delicious?
-    # YOUR CODE HERE
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(name, calories, flavor)
-    # YOUR CODE HERE
+    @flavor = flavor
+    super(name, calories)
   end
   
   def delicious?
-    # YOUR CODE HERE
+      flavor != "black licorice"
   end
 end
