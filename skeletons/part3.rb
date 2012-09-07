e29e3a48740f0c3565de467dd15992a66d830ee8
Peter Tsoi <petertsoi@gmail.com>
2012-09-06 18:31:03 -0700

    First pass at part 3: anagrams

--- skeletons/part3.rb
+++ skeletons/part3.rb
@@ -1,3 +1,7 @@
 def combine_anagrams(words)
- # YOUR CODE HERE
+  groups = Hash.new { |hash, key| hash[key] = [] }
+  words.each do |word|
+    groups[word.downcase.chars.sort.join] << word
+  end
+  groups.values
 end
