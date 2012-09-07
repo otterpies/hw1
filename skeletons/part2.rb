defb033732ca0e2a2a4c3fddac445b608300388e
Peter Tsoi <petertsoi@gmail.com>
2012-09-06 18:01:40 -0700

    First pass at part 2: rock paper scissors

--- skeletons/part2.rb
+++ skeletons/part2.rb
@@ -2,13 +2,30 @@ class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
 def rps_result(m1, m2)
-  # YOUR CODE HERE
+  legalMoves = ['r', 'p', 's']
+  raise NoSuchStrategyError unless legalMoves.include? m1.downcase and legalMoves.include? m2.downcase
+  wins = "rr", "rs", "pp", "pr", "ss", "sp"
+  if wins.include? (m1 + m2).downcase
+    return 0
+  else
+    return 1
+  end
 end
 
 def rps_game_winner(game)
-  # YOUR CODE HERE
+  raise WrongNumberOfPlayersError unless game.length == 2
+  game[rps_result(game[0][1], game[1][1])]
 end
 
 def rps_tournament_winner(tournament)
-  # YOUR CODE HERE
+  if isGame? tournament
+    rps_game_winner(tournament)
+  else
+    rps_game_winner([ rps_tournament_winner(tournament[0]),
+                      rps_tournament_winner(tournament[1]) ])
+  end
+end
+
+def isGame?(game)
+  return game[0][0].is_a?(String)
 end
