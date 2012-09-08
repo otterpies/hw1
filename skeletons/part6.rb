c4b718a9acbf10f8389c4873a59c77b145a5f2d6
Peter Tsoi <petertsoi@gmail.com>
2012-09-08 11:44:03 -0700

    First pass at part 6: open classes

--- skeletons/part6.rb
+++ skeletons/part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'dollar' => 1.0, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,12 +8,22 @@ class Numeric
       super
     end
   end
+  def in(currency)
+    singular_currency = currency.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    end
+  end
 end
 
 class String
-  # YOUR CODE HERE
+  def palindrome?()
+    self.downcase.gsub(/\W/, '') == self.downcase.gsub(/\W/, '').reverse
+  end
 end
 
 module Enumerable
-  # YOUR CODE HERE
+  def palindrome?()
+    self.respond_to? :reverse and self == self.reverse
+  end
 end
